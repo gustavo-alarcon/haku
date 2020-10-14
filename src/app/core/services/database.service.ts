8fa1672d74c36514998531eabec2d7e60afa9ecb
Gustavo Alarcon <galarcon@meraki-s.com>
2020-10-13 23:10:48 -0500

    deploy 1.0.1r

--- src/app/core/services/database.service.ts
+++ src/app/core/services/database.service.ts
@@ -19,7 +19,7 @@ import { StoreSale } from '../models/storeSale.model';
   providedIn: 'root'
 })
 export class DatabaseService {
-  public version: string = 'V1.0.0r';
+  public version: string = 'V1.0.1r';
   public isOpen: boolean = false;
   public isAdmin: boolean = false;
 
