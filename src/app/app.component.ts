c96ceea690184a2ee9315522686a70f669cd2713
Gustavo Alarcon <galarcon@meraki-s.com>
2020-10-13 23:19:54 -0500

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,6 @@ import { UpdateDialogComponent } from './shared-dialogs/update-dialog/update-dia
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = '24multiservicios';
   version$: Observable<Config>
 
   constructor(
